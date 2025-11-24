512c3f63959d3f0b59ac828275eb36836b7aff30
Shahid-Ahmed-S <shahidaug08@gmail.com>
2025-11-24 23:41:41 +0530

    Add files via upload

---
+++ src/com/JavaBasics/Day07/NumberSquare.java
@@ -0,0 +1,21 @@
+package com.JavaBasics.Day07;
+import java.util.Scanner;
+
+public class NumberSquare {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         System.out.print("Enter number of rows: ");
+         int n = sc.nextInt();
+         for (int i = 1; i <= n; i++) {
+             for (int j = 1; j <= n; j++) {
+                 if (i == 1 || i == n || j == 1 || j == n)
+                     System.out.print("1");
+                 else
+                     System.out.print(" ");
+             }
+             System.out.println();
+         }
+     }
+}
+
+
